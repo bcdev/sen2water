fix of nan-aware averaging and interpolation of reflectances at swath border

sen2water/msiresampling/downsampling.py
@@ -28,6 +28,7 @@ class Downsampling(BlockAlgorithm):
             "majority", "detectormean"]='mean',
         factor: int=6,
         is_azimuth_angle: bool=False,
+        is_reflectance: bool=False,
     ) -> np.ndarray:
         """Downsampling from e.g. 10m to 60m by mean or other aggregators"""
         data = inputs[0]
@@ -47,11 +48,26 @@ class Downsampling(BlockAlgorithm):
         elif mode == 'majority':  # for detector index
             result = np.round(np.mean(pixelcontributions, axis=0)).astype(np.uint8)
         elif mode == 'mean':
-            result = np.mean(pixelcontributions, axis=0)
+            if is_reflectance:
+                pixelcontributions = pixelcontributions.astype(np.float32)
+                pixelcontributions[pixelcontributions==0] = np.nan
+                result = np.nanmean(pixelcontributions, axis=0).astype(np.uint16)
+            else:
+                result = np.mean(pixelcontributions, axis=0)
         elif mode == 'median':
-            result = np.median(pixelcontributions, axis=0)
+            if is_reflectance:
+                pixelcontributions = pixelcontributions.astype(np.float32)
+                pixelcontributions[pixelcontributions==0] = np.nan
+                result = np.nanmedian(pixelcontributions, axis=0).astype(np.uint16)
+            else:
+                result = np.median(pixelcontributions, axis=0)
         elif mode == 'min' or mode == 'flagand':
-            result = np.min(pixelcontributions, axis=0)
+            if is_reflectance:
+                pixelcontributions = pixelcontributions.astype(np.float32)
+                pixelcontributions[pixelcontributions==0] = np.nan
+                result = np.nanmin(pixelcontributions, axis=0).astype(np.uint16)
+            else:
+                result = np.min(pixelcontributions, axis=0)
         elif mode == 'max' or mode == 'flagor':
             result = np.max(pixelcontributions, axis=0)
         elif mode == 'flagmedianand':

sen2water/msiresampling/upsampling.py
@@ -28,6 +28,7 @@ class Upsampling(OverlapAlgorithm):
         src_image_shape: Tuple[int, int],
         src_image_chunksize: Tuple[int, int],
         is_azimuth_angle: bool=False,
+        is_reflectance: bool=False,
         block_id: Tuple[int, int],
     ) -> np.ndarray:
         """Upsampling from e.g. 60m to 10m by filling or interpolation"""
@@ -91,13 +92,24 @@ class Upsampling(OverlapAlgorithm):
                 reference = src_data[1, 1]
                 src_data = (src_data - reference + 540.0) % 360.0 - 180.0
 
-            # 2-D interpolation using numpy broadcasting
-            result = (
+            if is_reflectance:
+                src_data = src_data.astype(np.float32)
+                src_data[src_data==0] = np.nan
+                result = np.nansum(np.stack([
+                    (1 - wy) * (1 - wx) * src_data[y_tp, x_tp],
+                    (1 - wy) * wx * src_data[y_tp, x_tp + 1],
+                    wy * (1 - wx) * src_data[y_tp + 1, x_tp],
+                    + wy * wx * src_data[y_tp + 1, x_tp + 1]
+                ]), axis=0).astype(src_data.dtype)
+
+            else:
+                # 2-D interpolation using numpy broadcasting
+                result = (
                     (1 - wy) * (1 - wx) * src_data[y_tp, x_tp]
                     + (1 - wy) * wx * src_data[y_tp, x_tp + 1]
                     + wy * (1 - wx) * src_data[y_tp + 1, x_tp]
                     + wy * wx * src_data[y_tp + 1, x_tp + 1]
-            ).astype(src_data.dtype)
+                ).astype(src_data.dtype)
 
             if is_azimuth_angle:
                 result = (result + reference + 540.0) % 360.0 - 180.0