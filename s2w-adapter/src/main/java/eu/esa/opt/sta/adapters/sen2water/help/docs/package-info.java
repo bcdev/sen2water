756fc5bb54a612ddff44bca961123448897fd333
Marco <info@eomasters.org>
2025-07-10 15:53:12 +0200

    initial Sen2Water adapter module

---
+++ s2w-adapter/src/main/java/eu/esa/opt/sta/adapters/sen2water/help/docs/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2025 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+@HelpSetRegistration(helpSet = "help.hs", position = 4330)
+package eu.esa.opt.sta.adapters.sen2water.help.docs;
+
+import eu.esa.snap.netbeans.javahelp.api.HelpSetRegistration;
