3b1ce22797681703ff0f8d75c6b11f656e6f223c
Marco <info@eomasters.org>
2025-07-15 10:07:39 +0200

    new gpt.bat file
    it sets the -Duser.home to prevent loading module updates and
    sets -Dsnap.dataio.reader.tileHeight=%blocksize% -Dsnap.dataio.reader.tileWidth=%blocksize%
    which were not effective before. These need to be set as VM parameters and not as program parameters. -J-D is also not working. Only works with the original gpt.exe.

---
+++ resources/patches/gpt.bat
@@ -0,0 +1,39 @@
+:: minimal gpt script for sen2water on Windows
+:: adds idepix and c2rcc to classpath
+
+@echo off
+setlocal
+
+set wd=%cd%
+cd %~dp0%\..\..\..
+set s2wdir=%cd%
+cd %wd%
+
+if "%blocksize%" == "" (
+  set blocksize=610
+	echo block size set to %blocksize%
+) else (
+  echo block size already set to %blocksize%
+)
+
+:: set PATH=%PATH%;%s2wdir%\lib\snap\snap\modules\lib\x86\jhdf5.dll
+:: dir %s2wdir%\lib\snap\snap\modules\lib\x86\*
+
+::echo "using java at %s2wdir%\lib\jre"
+::echo "using snap at %s2wdir%\lib\snap"
+::echo "working dir   %wd%"
+
+::-XshowSettings:properties
+
+%s2wdir%\lib\jre\bin\java ^
+-cp %s2wdir%\lib\snap\snap\modules\*;%s2wdir%\lib\idepix\*;%s2wdir%\lib\c2rcc\* ^
+-Duser.home=%s2wdir%\temp ^
+-Djava.awt.headless=true ^
+-Xmx6G ^
+-Dsnap.mainClass=org.esa.snap.core.gpf.main.GPT ^
+-Dsnap.dataio.reader.tileHeight=%blocksize% -Dsnap.dataio.reader.tileWidth=%blocksize% ^
+-Dsnap.home=%s2wdir%\lib\snap ^
+-Dsnap.userdir=%s2wdir% ^
+-Dsnap.cachedir=%wd%\cache ^
+-Dsnap.log.level=ERROR ^
+org.esa.snap.runtime.Launcher %*
