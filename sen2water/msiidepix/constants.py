f065708764edca15dd1bfba0526a80efa7c8530b
Olaf Danne <olaf.danne@brockmann-consult.de>
2026-08-04 17:40:53 +0200

    msi idepix: continued impl of spectral tests

--- sen2water/msiidepix/constants.py
+++ sen2water/msiidepix/constants.py
@@ -26,23 +26,50 @@ class IdepixMsiConstants(object):
 
     GCL_THRESH_DEFAULT = -0.11
     VISBRIGHT_THRESH = 0.12
+    TCL_THRESH = 0.085
 
-    FLAG_MASK_UNTRUE: int = np.uint8(1)
+    FLAG_MASK_INVALID: int = np.int32(1)
     """The flag value of the invalid pixel mask."""
-    FLAG_MASK_CLOUD: int = np.uint8(2)
+    FLAG_MASK_CLOUD: int = np.int32(2)
     """The flag value of the cloud mask."""
-    FLAG_MASK_CLOUD_AMBIGUOUS: int = np.uint8(4)
+    FLAG_MASK_CLOUD_AMBIGUOUS: int = np.int32(4)
     """The flag value of the cloud ambiguous mask."""
-    FLAG_MASK_CLOUD_SURE: int = np.uint8(8)
+    FLAG_MASK_CLOUD_SURE: int = np.int32(8)
     """The flag value of the cloud sure mask."""
-    FLAG_MASK_CLOUD_BORDER: int = np.uint8(16)
+    FLAG_MASK_CLOUD_BORDER: int = np.int32(16)
     """The flag value of the cloud border mask."""
-    FLAG_MASK_CLOUD_SHADOW: int = np.uint8(32)
+    FLAG_MASK_CLOUD_SHADOW: int = np.int32(32)
     """The flag value of the cloud shadow mask."""
-    FLAG_MASK_CLEAR_SNOW: int = np.uint8(64)
+    FLAG_MASK_CLEAR_SNOW: int = np.int32(64)
     """The flag value of the clear-sky snow and ice mask."""
-    FLAG_MASK_MOUNTAIN_SHADOW: int = np.uint16(2048)
+    FLAG_MASK_BRIGHT: int = np.int32(128)
+    """The flag value of the bright mask."""
+    FLAG_MASK_WHITE: int = np.int32(256)
+    """The flag value of the white mask."""
+    FLAG_MASK_COASTLINE: int = np.int32(512)
+    """The flag value of the coastline mask."""
+    FLAG_MASK_LAND: int = np.int32(1024)
+    """The flag value of the land mask."""
+    FLAG_MASK_CIRRUS_SURE: int = np.int32(2048)
+    """The flag value of the cirrus sure mask."""
+    FLAG_MASK_CIRRUS_AMBIGUOUS: int = np.int32(4096)
+    """The flag value of the cirrus ambiguous mask."""
+    FLAG_MASK_CLEAR_LAND: int = np.int32(8192)
+    """The flag value of the clear land mask."""
+    FLAG_MASK_CLEAR_WATER: int = np.int32(16384)
+    """The flag value of the clear water mask."""
+    FLAG_MASK_WATER: int = np.int32(32768)
+    """The flag value of the water mask."""
+    FLAG_MASK_BRIGHTWHITE: int = np.int32(65536)
+    """The flag value of the brightwhite mask."""
+    FLAG_MASK_VEG_RISK: int = np.int32(131072)
+    """The flag value of the veg risk mask."""
+    FLAG_MASK_MOUNTAIN_SHADOW: int = np.int32(262144)
     """The flag value of the mountain shadow mask"""
+    FLAG_MASK_POTENTIAL_SHADOW: int = np.int32(524288)
+    """The flag value of the potential shadow mask"""
+    FLAG_MASK_CLUSTERED_CLOUD_SHADOW: int = np.int32(524288)
+    """The flag value of the clustered cloud shadow mask"""
 
     bands = [
         "b01",
@@ -73,6 +100,22 @@ class IdepixMsiConstants(object):
         "b10": 60,
         "b11": 20,
         "b12": 20,
+    }
+
+    wvls = {
+        443.0,     # B1
+        490.0,     # B2
+        560.0,     # B3
+        665.0,     # B4
+        705.0,     # B5
+        740.0,     # B6
+        783.0,     # B7
+        842.0,     # B8
+        865.0,     # B8A
+        945.0,     # B9
+        1375.0,    # B10
+        1610.0,    # B11
+        2190.0     # B12
     }
 
     flag_band_prefixes = [
